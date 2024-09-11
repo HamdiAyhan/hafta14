b44c27b0ff1e405360184abaf0dd70cf3f27c526
HamdiAyhan <hamdiminer1@gmail.com>
2024-09-11 15:24:15 +0300

    routergiriş yapıldı outlet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./ekranlar/Layout";
+import {Anasayfa} from "./ekranlar/Anasayfa";
+import {Blog} from "./ekranlar/Blog";
+import {Contact} from "./ekranlar/Contact";
+import {Nopage} from "./ekranlar/Nopage";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Anasayfa />} />
+        <Route path="blogs" element={<Blog />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<Nopage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 

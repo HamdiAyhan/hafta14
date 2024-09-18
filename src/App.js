032b3bb4bb39a7f51663ef2a292661d2992dd579
HamdiAyhan <hamdiminer1@gmail.com>
2024-09-18 20:09:57 +0300

    api ile çalışması sağlandı

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import AddTodoForm from "./components/AddTodoForm";
-import TodoList from "./components/TodoList";
-import TotalCompleteItems from "./components/TotalCompleteItems";
+import {useDispatch} from 'react-redux';
+import AddTodoForm from './components/AddTodoForm';
+import TodoList from './components/TodoList';
+import TotalCompleteItems from './components/TotalCompleteItems';
+import { useEffect } from 'react';
+import {todosDoldur} from './redux/todoSlice';
+
+function App () {
+  const vekilFonksiyon = useDispatch ();
 
 
-function App() {
+      async function verial () {
+      const istek = await fetch ('https://jsonplaceholder.typicode.com/todos');
+      const veri = await istek.json ();
+      vekilFonksiyon (todosDoldur ({veri: veri.slice (0, 20)}));
+    }
+    verial ();
+
+ 
+ 
+ 
   return (
     <div className="App">
-<h1>To Do App</h1>
-<AddTodoForm/>
-<TodoList/>
-<TotalCompleteItems/>
+      <h1>To Do App</h1>
+      <AddTodoForm />
+      <TodoList />
+      <TotalCompleteItems />
     </div>
   );
 }
--- src/components/TotalCompleteItems.js
+++ src/components/TotalCompleteItems.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 
 const TotalCompleteItems = () => {
-    const todos = useSelector((state) =>
-        state.todos.filter((todo) => todo.completed === true)
-    );
+  const todos = useSelector (state =>
+    state.todos.filter (todo => todo.completed === true)
+  );
 
-    return <h4 className='mt-3'>Tamamlanan Görevler: {todos.length}</h4>;
+  return <h4 className="mt-3">Tamamlanan Görevler: {todos.length}</h4>;
 };
 
 export default TotalCompleteItems;
